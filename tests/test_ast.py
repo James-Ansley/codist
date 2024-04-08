dad1990562c0ca0819da2c85f4309a1f4ef0e224
James Ansley <31982662+James-Ansley@users.noreply.github.com>
2024-04-08 12:15:03 +1200

    Update test_ast.py

--- tests/test_ast.py
+++ tests/test_ast.py
@@ -1,5 +1,7 @@
-from codist.ast import parse_ast_silhouette
+from ast import parse
+
 from codist import t
+from codist.ast import basic_ast, parse_ast_silhouette
 
 
 def test_empty_code_is_parsed_to_a_module_silhouette():
@@ -11,3 +13,16 @@ def test_simple_expression_silhouette():
         "Module",
         t("Assign", t("Name", t("Store")), t("Constant")),
     )
+
+
+def test_basic_ast_makes_no_distinction_between_empty_literals_or_constructors():
+    assert basic_ast(parse("[]", mode="eval"), {}) \
+           == basic_ast(parse("list()", mode="eval"), {})
+    assert basic_ast(parse("{}", mode="eval"), {}) \
+           == basic_ast(parse("dict()", mode="eval"), {})
+    assert basic_ast(parse("()", mode="eval"), {}) \
+           == basic_ast(parse("tuple()", mode="eval"), {})
+
+
+def test_basic_ast_reassigns_names():
+    assert basic_ast(parse("x", mode="eval"), {"x": 1})[1][0][0] == "Name(1)"
